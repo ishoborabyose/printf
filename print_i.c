aac9a51f34a471c771b8ba9f28c81a175e3900d8
John-Bugingo <johnbugingo12@gmail.com>
2022-04-19 16:54:43 -0700

    format specifier of i

--- print_i.c
+++ print_i.c
@@ -5,6 +5,7 @@
  * @l: va_list of arguments from _printf
  * @f: pointer to the struct flags determining
  * if a flag is passed to _printf
+ *
  * Return: number of char printed
  */
 int print_int(va_list l, flags_t *f)
